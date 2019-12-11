03149b713d29357f296aaa42513f86b4288928cc
charli <charli.pacquet@gmail.com>
2019-12-11 15:24:38 +0100

    comptage de point beta

---
+++ Assets/marquer_un_panier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.UIWidgets.painting;
+using UnityEngine;
+
+public class marquer_un_panier : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("ballon"))
+        {
+            Debug.Log("un pointdddd");
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ballon"))
+        {
+            Debug.Log("un point");
+        }
+    }
+}

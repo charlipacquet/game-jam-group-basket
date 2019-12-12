a787ce9d6b5518d00213d6443bd3219b86b4d6d2
charli <charli.pacquet@gmail.com>
2019-12-12 12:04:48 +0100

    ramasse la balle

--- Assets/ramasse_la_baballe.cs
+++ Assets/ramasse_la_baballe.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ramasse_la_baballe : MonoBehaviour
 {
-    public void OnTriggerEnter(Collider other)
+    public GameObject spawnballe;
+
+    public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("ramasse la balle");
+            Instantiate(spawnballe, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(spawnballe);
         }
     }
-}
+}   

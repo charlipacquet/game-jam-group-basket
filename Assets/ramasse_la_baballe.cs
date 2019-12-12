13373c0a9d9c265bff754ebb95409ad0dc5336c7
charli <charli.pacquet@gmail.com>
2019-12-12 13:31:24 +0100

    ramasse la baballe

--- Assets/ramasse_la_baballe.cs
+++ Assets/ramasse_la_baballe.cs
@@ -9,9 +9,9 @@ public class ramasse_la_baballe : MonoBehaviour
 
     public void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !other.gameObject.GetComponentInParent<ProjectileStandard>())
         {
-            Debug.Log("ramasse la balle");
+            Debug.Log("ramasse la balle " + other.gameObject.tag);
             Instantiate(spawnballe, transform.position, transform.rotation);
             Destroy(gameObject);
         }
